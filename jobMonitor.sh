d0de10cfe01192ff612b0264245eff437c64b659
selterb <selterb01@gmail.com>
2018-07-27 05:48:27 -0400

    1.01

--- jobMonitor.sh
+++ jobMonitor.sh
@@ -32,31 +32,34 @@ if lastRun == '':
 print "Waiting for Cohesity Protection Job To Run..."
 
 while True:
-    jobId = api('get', 'protectionJobs?names=' + urllib.quote_plus(JOBNAME))[0]['id']
-    runs = api('get','protectionRuns?jobId=' + str(jobId))
-    if 'startTimeUsecs' in runs[0]['backupRun']['stats']:
-        latestRun = runs[0]['backupRun']['stats']['startTimeUsecs']
-    if int(latestRun) > int(lastRun): #job started since we last checked
-        runURL = 'protectionRuns?startedTimeUsecs=%s&jobId=%s' % (latestRun,jobId)
-        #email start alert
-        sendMessage(STARTMESSAGE + usecsToDate(latestRun))
-        print JOBNAME + ' started at ' + usecsToDate(latestRun)
-        stillRunning = True
-        while (stillRunning):
-            state = api('get',runURL)
-            if 'endTimeUsecs' in state[0]['backupRun']['stats']:
-                result = state[0]['backupRun']['status']
-                #email result
-                if result == 'kSuccess':
-                    COMPLETEDMESSAGE = ENDMESSAGE.replace('Ended', 'Completed Successfully')
-                else:
-                    COMPLETEDMESSAGE = ENDMESSAGE.replace('Ended', 'UNSUCCESSFUL!!')    
-                sendMessage(COMPLETEDMESSAGE + result + ' at ' + usecsToDate(state[0]['backupRun']['stats']['endTimeUsecs']))
-                print JOBNAME + ' ended with ' + result + ' at ' + usecsToDate(state[0]['backupRun']['stats']['endTimeUsecs'])
-                stillRunning = False
-            time.sleep(SLEEPTIME)
-        lastRun = latestRun
-        f = open(RUNFOLDER + 'lastrun','w')
-        f.write(str(lastRun))
-        f.close()
-    time.sleep(SLEEPTIME)
+    try:
+        jobId = api('get', 'protectionJobs?names=' + urllib.quote_plus(JOBNAME))[0]['id']
+        runs = api('get','protectionRuns?jobId=' + str(jobId))
+        if 'startTimeUsecs' in runs[0]['backupRun']['stats']:
+            latestRun = runs[0]['backupRun']['stats']['startTimeUsecs']
+        if int(latestRun) > int(lastRun): #job started since we last checked
+            runURL = 'protectionRuns?startedTimeUsecs=%s&jobId=%s' % (latestRun,jobId)
+            #email start alert
+            sendMessage(STARTMESSAGE + usecsToDate(latestRun))
+            print JOBNAME + ' started at ' + usecsToDate(latestRun)
+            stillRunning = True
+            while (stillRunning):
+                state = api('get',runURL)
+                if 'endTimeUsecs' in state[0]['backupRun']['stats']:
+                    result = state[0]['backupRun']['status']
+                    #email result
+                    if result == 'kSuccess':
+                        COMPLETEDMESSAGE = ENDMESSAGE.replace('Ended', 'Completed Successfully')
+                    else:
+                        COMPLETEDMESSAGE = ENDMESSAGE.replace('Ended', 'UNSUCCESSFUL!!')    
+                    sendMessage(COMPLETEDMESSAGE + result + ' at ' + usecsToDate(state[0]['backupRun']['stats']['endTimeUsecs']))
+                    print JOBNAME + ' ended with ' + result + ' at ' + usecsToDate(state[0]['backupRun']['stats']['endTimeUsecs'])
+                    stillRunning = False
+                time.sleep(SLEEPTIME)
+            lastRun = latestRun
+            f = open(RUNFOLDER + 'lastrun','w')
+            f.write(str(lastRun))
+            f.close()
+        time.sleep(SLEEPTIME)
+    except:
+        time.sleep(SLEEPTIME)
